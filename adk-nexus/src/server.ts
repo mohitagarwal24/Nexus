b1968d7926f25ccfb2af96dec97ae43c24a3b3a0
mohitagarwal24 <agarwalmohit545@gmail.com>
2025-12-12 14:30:17 +0000

    Updated cors policy with allowed headers

--- adk-nexus/src/server.ts
+++ adk-nexus/src/server.ts
@@ -53,7 +53,7 @@ const corsOptions = {
     },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-api-key', 'x-session-id']
 };
 
 app.use(cors(corsOptions));
